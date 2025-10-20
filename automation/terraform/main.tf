aa6da346b78112110c9b8d36a3406b39f0d8f94c
Laszlo Igaz <laszlo.igaz.epam+ah@gmail.com>
2025-10-20 09:54:53 +0200

    Small automation fix

--- automation/terraform/main.tf
+++ automation/terraform/main.tf
@@ -44,7 +44,7 @@ module "cloudx_task_03" {
   acr_managed_identity_principal_id = module.permanent_resources.managed_identity_principal_id
   acr_managed_identity_client_id    = module.permanent_resources.managed_identity_client_id
   acr_managed_identity_id           = module.permanent_resources.managed_identity_id
-  docker_image_tag                  = "build-9"
+  docker_image_tag                  = "build-14"
   app_plan_web_services_name        = var.app_plan_web_services_name
   app_plan_web_services_location    = var.app_plan_web_services_location
 }
